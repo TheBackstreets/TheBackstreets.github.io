6d146cca5434ede716827799abb84994c968c3a4
misclassify <misclassifyy@gmail.com>
2025-02-02 17:45:25 -0800

    Update FAQ.js

--- js/FAQ.js
+++ js/FAQ.js
@@ -7,16 +7,20 @@ document.querySelectorAll('.fheader').forEach(button => {
         if (button.classList.contains('factive')) {
             accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
             button.querySelector('.icon').textContent = '-'; // Change + to -
+
         } else {
             accordionContent.style.maxHeight = 0;
-            button.querySelector('.icon').textContent = '+'; // Change - to +
+            button.querySelector('.icon').textContent = '+'; // Change + to -
+
         }
 
+        // Close other open accordion items
         document.querySelectorAll('.fheader').forEach(otherButton => {
             if (otherButton !== button) {
                 otherButton.classList.remove('factive');
                 otherButton.nextElementSibling.style.maxHeight = 0;
                 otherButton.querySelector('.icon').textContent = '+'; // Change - to +
+
             }
         });
     });
