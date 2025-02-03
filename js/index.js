0ec40e6656685c4e9723f4a9b1b714a0f110539c
misclassify <misclassifyy@gmail.com>
2025-02-02 17:45:53 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,14 @@
-var _____WB$wombat$assign$function_____ = function(name) {
-  return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name];
-};
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
 
-if (!self.__WB_pmw) {
-  self.__WB_pmw = function(obj) {
-    this.__WB_source = obj;
-    return this;
-  };
-}
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
 
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
 
-  const menuToggle = document.querySelector('.toggle');
-  const showcase = document.querySelector('.showcase');
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+  customizeTranslateWidget();
 
-  menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    showcase.classList.toggle('active');
-  });
-
-  function googleTranslateElementInit() {
-    new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
-    customizeTranslateWidget();
-  }
 }
